96da47421c06d1af1d1c495ed886496fa4f23b57
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-12-13 11:31:05 +0700

    Add project files.

---
+++ BankAccount/Bank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public delegate void Operation(string info);
+    class Bank
+    {
+        public event Operation AccountEvent;
+        public int Money { get; private set; }
+
+        public Bank(int money)
+        {
+            Money = money;
+        }
+
+        public void BalansInfo()
+        {
+            Console.WriteLine($"На вашем счету {Money} рублей");
+        }
+
+        public void InsertMoney(int money)
+        {
+            Money += money;
+            Console.ForegroundColor = ConsoleColor.Green;
+            AccountEvent?.Invoke($"* На счет поступило {money} рублей ");
+            Console.ResetColor();
+            BalansInfo();
+        }
+
+        public void TakeMoney(int money)
+        {
+            if (Money >= money)
+            {
+                Money -= money;
+                Console.ForegroundColor = ConsoleColor.Green;
+                AccountEvent?.Invoke($"* Со счета снято {money} рублей ");
+                Console.ResetColor();
+                BalansInfo();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                AccountEvent?.Invoke("* На вашем счете недостаточно средств");
+                Console.ResetColor();
+                BalansInfo();
+            }
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        public static event Operation Message;
+        static void Main()
+        {
+            int money = 2000;
+            Bank account = new Bank(money);
+            account.BalansInfo();
+            account.AccountEvent += Info;
+            Message += Console.WriteLine;
+            Message?.Invoke("Введите сумму которую хотите внести рублей ");
+            money = Convert.ToInt32(Console.ReadLine());
+            account.InsertMoney(money);
+            Message?.Invoke("Введите сумму которую хотите снять рублей ");
+            money = Convert.ToInt32(Console.ReadLine());
+            account.TakeMoney(money);
+        }
+        public static void Info(string info)
+        {
+            Console.WriteLine(info);
+        }
+    }    
+}
